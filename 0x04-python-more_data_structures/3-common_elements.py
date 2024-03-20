213f98810ed2137b55de2bbc63a9d2f6d9e16c9e
Ajayi Sunday Olusoji <ajayisunday64@gmail.com>
2024-03-20 15:41:15 -0700

    a function that returns a set of common elements in two sets.

---
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+
+def common_elements(set_1, set_2):
+    new_set_1 = set(set_1);
+    new_set_2 = set(set_2);
+    common_result = new_set_1 & new_set_2
+    return common_result
